e65436cf8b4fa9bb7feb35138e2e535bb7047404
Óscar San José <oscar@tuenti.com>
2018-04-19 17:27:02 +0200

    Revert "Remove flakes from deps, fails to install in certain setuptools versions"
    This reverts commit 6c3dc1b482c378c5396f348eb9ea1edd5f0d4370.

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
     url='https://github.com/eht16/python-logstash-async',
     keywords='logging logstash asynchronous',
     install_requires=['limits', 'six'],
+    setup_requires=['flake8'],
     include_package_data=True,
     classifiers=[
         'Development Status :: 4 - Beta',
